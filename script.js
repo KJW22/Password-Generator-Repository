c6e4fc4f46c1868b1d88d73069d63c937e85e399
Kevin Wang <kwang7@horizon.csueastbay.edubrew>
2020-06-18 19:05:58 -0700

    added if then statements for remaining three criteria variables and fixed bracket error in function on line 6

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 // Assignment Code
 
-
 var generateBtn = document.querySelector("#generate");
-generateBtn.onclick=function () {var passwordLength = prompt ("enter desired password length")};
-while (passwordLength>=8 && passwordLength<=128)
-
-// Code checks length inputted 
-  // If the inputted length is greater than or equal to 2 and less than or equal to 128
-    // Password length is accepted.
-    console.log("length accepted")
-  
-  // Otherwise 
-    // Log "specifies password length"
-  
-    {console.log("length must between 8-128 characters")}}
+generateBtn.onclick=function () 
 // Write password to the #password input
-function writePassword() {
+{function writePassword() 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -23,7 +11,18 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);}
 var el_down = document.getElementById("geeks"); 
-  
+var passwordLength = prompt ("enter desired password length");
+
+if (passwordLength>=8 && passwordLength<=128)
+
+// Code checks length inputted. 
+  // If the inputted length is greater than or equal to 2 and less than or equal to 128
+    // Password length is accepted.
+    {console.log("length accepted");}
+
+  // Otherwise 
+    // Log "specifies password length"
+  else {console.log("length must be between 8-128 characters"};
 /* Function to generate combination of password */ 
 function generateP() { 
     var passCritera= "upperCase, LowerCase, number, specialCharacter"; 
@@ -43,14 +42,22 @@ function generateP() {
 
 function gfg_Run() { 
     el_down.innerHTML = generateP(); 
-
+//var names for lines 47-61 stand for questions asked in prompts.
 var ucl = prompt ("use capital letters? Type y or n.");
- 
-If (ucl==="yes") {"UpperCase=ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-If (ucl === "no") {"UpperCase=undefined"}
+If (ucl === "yes"); {"UpperCase=ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+If (ucl === "no"); {"UpperCase=undefined"};
 
-var ulc= prompt ("use lowercase letters? Type y or n.")
-var un = prompt ("use numbers? y or n.")
-var usc = prompt ("use special characters?")
+var ulc = prompt ("use lowercase letters? Type y or n.");
+If (ulc === "yes"); {"lowerCase=abcdefghijklmnopqrstuvwxyz"};
+If (ulc === "no"); {"lowerCase=undefined"};
+
+var un = prompt ("use numbers? Type y or n.");
+If (un === "yes"); {"number=0123456789"};
+If (un === "no"); {"number=undefined"};
+
+var usc = prompt ("use special characters? Type y or n.");
+If (usc === "yes"); {"specialCharacter=#$%&'()*+,-./:;<=>?@[\]^_`{|}~"};
+If (usc === "no"); {"specialCharacter=undefined"};
+
 // what I want is a prompt for each of the four types of characters. I want to be able to exclude the variables users say no to, and include the ones they say yes to.  
 //I will exclude the variables by having them be undefined when the user says no, and use the original variables when they say yes. 
